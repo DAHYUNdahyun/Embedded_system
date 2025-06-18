1b0b61c2f661a6b44af5a0a9739171d1a404c537
Dahyundahyun <dahyun345@naver.com>
2025-06-18 15:02:48 +0900

    음식 이미지 여백 제거

--- crop_image.py
+++ crop_image.py
@@ -1,12 +1,12 @@
 from PIL import Image
-import os  # ✅ 꼭 필요
+import os
 
-folder = "assets/"
+folder = "assets/food/"
 
 for filename in os.listdir(folder):
-    if filename.endswith(".png") and "tama" in filename:
+    if filename.endswith(".png"):
         path = os.path.join(folder, filename)
         img = Image.open(path)
-        cropped = img.crop(img.getbbox())
-        cropped.save(path)  # 원본 덮어쓰기
+        cropped = img.crop(img.getbbox())  # 투명 여백 제거
+        cropped.save(path)  # 덮어쓰기
         print(f"✅ {filename} 여백 제거 완료")
